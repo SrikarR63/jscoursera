c1beba8035a19a1c0e46af762b0d9fad048df470
Srik <srikarrajagopalan2002@gmail.com>
2025-08-20 09:41:57 -0400

    Array Iteration Logic

---
+++ restaurantMenu/restaurant_menu.js
@@ -0,0 +1,19 @@
+const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
+const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
+const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+
+
+const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
+document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
+
+let mainCourseItem = '';
+mainCourseMenu.forEach((item, index) => {
+mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;});
+document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
+
+
+let dessertItem='';
+for(let x=0;x<dessertMenu.length;x++){
+    dessertItem += `<p>Item ${x + 1}: ${dessertMenu[x]}</p>`;
+}
+document.getElementById('dessertMenuItems').innerHTML = dessertItem;
