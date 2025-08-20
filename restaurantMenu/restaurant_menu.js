54fa3fddc6baf21f862679fc0b69fa03a9be4047
Srik <srikarrajagopalan2002@gmail.com>
2025-08-20 10:02:43 -0400

    Array Iteration Logic Practise Exercise

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,7 +1,9 @@
 const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
 const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
 const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
-
+const breakfastprice=[25,35,45,34];
+const maincourseprice=[25,35,45,34];
+const dessertprice=[25,35,45,34];
 
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
 document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
@@ -17,3 +19,10 @@ for(let x=0;x<dessertMenu.length;x++){
     dessertItem += `<p>Item ${x + 1}: ${dessertMenu[x]}</p>`;
 }
 document.getElementById('dessertMenuItems').innerHTML = dessertItem;
+
+
+const temp=breakfastprice.map((itemprice,index)=>
+    `<p>Item ${index+1}: ${breakfastMenu[index]} - ${itemprice} </p>`
+).join('');
+
+document.getElementById('breakfastMenuItems').innerHTML=temp;
