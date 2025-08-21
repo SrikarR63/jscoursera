fcc9f453704b14ad7ccf6311b123ddb436a2f0e7
Srik <srikarrajagopalan2002@gmail.com>
2025-08-21 09:10:22 -0400

    Added feature to Employee Management System

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,7 +1,7 @@
 const employees = [
-    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
+    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialization:'JavaScript' },
+    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000, specialization:'Python' },
+    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, specialization:'C++' },
     //... More employee records can be added here
   ];
 
@@ -40,3 +40,14 @@ function findEmployeeById(employeeId) {
       document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this ID';
      }
  }
+
+ function findempbyspec(specdetails){
+    const findspec=employees.find((employee)=>employee.specialization===specdetails);
+    if(findspec){
+        document.getElementById('employeesDetails').innerHTML=`<p>${findspec.name} - ${findspec.age} - ${findspec.department} - ${findspec.salary}</p>`;
+    }
+    else{
+        document.getElementById('employeesDetails').innerHTML=`<p>Could not find employee with ${specdetails} specialization</p>`;
+    }
+ }
+
