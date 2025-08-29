750c97695538b6758aeceb60c6a8120e3c458f92
Srik <srikarrajagopalan2002@gmail.com>
2025-08-29 15:56:38 -0400

    Added total length in result

--- speedAnalysis/speed_analysis.js
+++ speedAnalysis/speed_analysis.js
@@ -28,6 +28,7 @@ function endTest() {
     // Calculate time elapsed and words per minute (WPM)
     var timeElapsed = (endTime - startTime) / 1000; // in seconds
     var userTypedText = document.getElementById("userInput").value;
+    var usertextlength=userTypedText.length;
 
     // Split the text using regex to count words correctly
     var typedWords = userTypedText.split(/\s+/).filter(function (word) {
@@ -43,6 +44,7 @@ function endTest() {
     // Display the results
     var outputDiv = document.getElementById("output");
     outputDiv.innerHTML = "<h2>Typing Test Results:</h2>" +
+        "<p>Total Length: " + usertextlength + "</p>" +
         "<p>Words Typed: " + typedWords + "</p>" +
         "<p>Time Elapsed: " + timeElapsed.toFixed(2) + " seconds</p>" +
         "<p>Words Per Minute (WPM): " + wpm + "</p>";
