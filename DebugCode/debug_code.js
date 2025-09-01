79b712a318098ba735c59d16ca06c781de65643f
Srik <srikarrajagopalan2002@gmail.com>
2025-09-01 14:01:36 -0400

    Calculator functionality added

--- DebugCode/debug_code.js
+++ DebugCode/debug_code.js
@@ -3,11 +3,17 @@
         // Get user input from input fields
         let num1 = parseInt(document.getElementById('input1').value);
         let num2 = parseInt(document.getElementById('input2').value);
+        let op=document.getElementById('operator').value;
         // Check if inputs are valid numbers
         if (!isNaN(num1) && !isNaN(num2)) {
             // Perform the operation
-            let result = multiply(num1, num2);
-
+            let result=0;
+            if(num1%2==0){
+            result = multiply(num1, num2);
+            }
+            else{
+            result = arithmeticoper(num1,num2,op);
+            }
             // Display the result
             displayResult(result);
         } else {
@@ -28,4 +34,28 @@
         const resultElement = document.getElementById('result');
         resultElement.textContent = `The result is: ${result}`;
     }
-        
+    
+
+function arithmeticoper(a,b,op){
+    debugger;
+    switch(op){
+        case '+': return (a+b);
+                  break;
+        case '-': return (a+b);
+                  break;
+        case '*': return (a*b);
+                  break;
+        case '/': try{
+                    if(b==0){
+                    throw new Error("Division by zero is not allowed");
+                    }
+                    return a/b;
+                }
+                catch(error){
+                    console.error("Error found",error.message);
+                }                  
+                break;
+        default: console.log("Choose available operators!");
+                  break;
+    }
+}
