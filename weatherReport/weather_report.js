570fd9f2d0d4c14ae64bb632ca5ce9ecc078e306
Srik <srikarrajagopalan2002@gmail.com>
2025-09-17 16:34:10 -0400

    Added Geo-coordinates feature

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -1,22 +1,36 @@
 function showweatherDetails(event) {
       event.preventDefault();
       const city = document.getElementById('city').value;
-      const apiKey = 'YOUR_API_KEY'; // Replace 'YOUR_API_KEY' with your actual API key
-      const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+      const lat=document.getElementById('latitude').value;
+      const lon=document.getElementById('longitude').value;
+
+      const apiKey = 'API_KEY'; // Replace 'YOUR_API_KEY' with your actual API key
+      const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
 
       fetch(apiUrl)
         .then(response => response.json())
-        .then(data => {
+        /*.then(data => {
           const weatherInfo = document.getElementById('weatherInfo');
           weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
                                   <p>Temperature: ${data.main.temp} &#8451;</p>
                                   <p>Weather: ${data.weather[0].description}</p>`;
+        })*/
+        .then(data => {
+            console.log("API Data:", data);
+            const latlonelem=document.getElementById('latloninfo');
+            latlonelem.innerHTML=`<h2>Geo co-ordinates are</h2>
+                                  <p>Latitude is: ${data.coord.lat}</p>
+                                  <p>Longitude is: ${data.coord.lon}</p>
+                                  <p>The place is: ${data.name}</p>`;
         })
+        .catch(error => {console.error("Error:", error);});
+        /*
         .catch(error => {
             console.error('Error fetching weather:', error);
             const weatherInfo = document.getElementById('weatherInfo');
             weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`;
           });
+          */
 }
 
 document.getElementById('weatherForm').addEventListener('submit',showweatherDetails );
